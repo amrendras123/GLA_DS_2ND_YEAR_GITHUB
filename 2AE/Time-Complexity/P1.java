4b787730803874c58c90d8f37d8564e42deb0dce
amrendra <samrendra497@gmail.com>
2024-03-14 12:11:56 +0530

    14-03-2024

---
+++ 2AE/Time-Complexity/P1.java
@@ -0,0 +1,36 @@
+/**
+ * P1
+ */
+public class P1 {
+
+    public static void main(String[] args) {
+        
+        // long start=System.currentTimeMillis();
+
+        // for(int i=0;i<1000000;i++){
+
+        // }
+        // long end=System.currentTimeMillis();
+        // System.out.println(end-start);
+
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        int a=10;
+        int b=1;
+        System.out.println(a+b);
+
+     int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+           System.out.println("hi");
+            }
+        }
+
+
+    }
+}
