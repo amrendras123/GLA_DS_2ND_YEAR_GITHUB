7c096c64530980c23d1f413c5be3b7fd5a67d558
amrendra <samrendra497@gmail.com>
2024-04-08 11:03:22 +0530

    08-04-2024

---
+++ 2AD/LinkedList/Lect-3/LinkedListClient.java
@@ -0,0 +1,21 @@
+public class LinkedListClient {
+    
+
+    public static void main(String[] args)throws Exception {
+        LinkedList ll=new LinkedList();
+        ll.addFirst(5);
+        ll.addFirst(10);
+        ll.addFirst(20);
+        ll.addLast(12);
+        ll.addFirst(31);
+        ll.addLast(13);
+        ll.addAt(3, 16);
+        ll.addAt(0, 100);
+        // ll.removeFirst();
+        // ll.removeLast();
+        // System.out.println(ll.removeAt(5));
+        System.out.println(ll.removeFirst());
+        System.out.println(ll.removeLast());
+        ll.Display();
+    }
+}
---
+++ 2AD/LinkedList/Lect-3/Middle_Of_LL.java
@@ -0,0 +1,14 @@
+public class Middle_Of_LL {
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow=head;
+            ListNode fast=head;
+            while(fast!=null&&fast.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            return slow;
+            
+        }
+    }
+}
